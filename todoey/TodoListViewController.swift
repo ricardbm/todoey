098986584a91b197e253d1526ca78fabe8c1cf85
Ricard <Ricardbm2016@gmail.com>
2018-12-10 22:22:54 +0100

    stored the list into userdefaults.standard

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -11,9 +11,18 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "buy Eggos", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+            print(itemArray)
+        }
+    
     }
     
     //MARK - Tableview Datasource Methods
@@ -53,6 +62,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item button on our UIALERT
             self.itemArray.append(textField.text!)//        once selected, it gets a checkmark
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
